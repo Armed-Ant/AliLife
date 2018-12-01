85049612ea2ce5b325092452ae1e309363538c0d
Auser <986661291@qq.com>
2018-12-01 10:58:58 +0800

    Basic framework

---
+++ LifeBase/src/main/java/com/ali/life/LifeBase/LifeBaseApplication.java
@@ -0,0 +1,12 @@
+package com.ali.life.LifeBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeBaseApplication.class, args);
+	}
+}
